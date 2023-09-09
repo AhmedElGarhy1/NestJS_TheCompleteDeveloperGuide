d400173c7515638a5c91a31392a0586317913392
AhmedElGarhy1 <gemater.g@gmail.com>
2023-09-09 20:04:52 +0200

    update conflict

--- car-value/src/users/users.controller.ts
+++ car-value/src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-<<<<<<< HEAD
   Get,
   Post,
   Delete,
@@ -53,51 +52,5 @@ export class UsersController {
   update(@Param('id') id: number, @Body() userData: UpdateUserDto) {
     const user = this.userService.update(id, userData);
     return user;
-=======
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-  UseInterceptors,
-  ClassSerializerInterceptor,
-} from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto';
-import { UsersService } from './users.service';
-import { UpdateUserDto } from './dtos/update-user.dto';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
-import { UserDto } from './dtos/user.dto';
-
-@Controller('auth')
-@Serialize(UserDto)
-export class UsersController {
-  constructor(private userService: UsersService) {}
-
-  @Post('singup')
-  createUser(@Body() userData: CreateUserDto) {
-    this.userService.create(userData.email, userData.password);
-    return userData;
-  }
-
-  @Get()
-  findAllUsers(@Query('email') email: string) {
-    return this.userService.find(email);
-  }
-
-  @Get(':id')
-  findOneUser(@Param('id') id: string) {
-    return this.userService.findOne(parseInt(id));
-  }
-
-  @Patch(':id')
-  updateUser(@Param('id') id: string, @Body() userData: UpdateUserDto) {
-    return this.userService.update(parseInt(id), userData);
-  }
-
-  @Delete(':id')
-  deleteUser(@Param('id') id: string) {
-    return this.userService.remove(parseInt(id));
->>>>>>> 5accf3c4cdd00d9c3ab7f694f397a23d99a68f2f
   }
 }
